8cfce43197c3e3158ffaa8e21533e4a54c3b63ce
Shah Alam <mdalam127671@gmail.com>
2025-08-11 23:05:24 +0600

    1 problem solve

---
+++ problem-solve.js
@@ -0,0 +1,6 @@
+// 1. A single line of input containing integer , the number of times to print "Hello World".
+// let n = 4;
+//  for (let i =0; i<n; i++){
+//     console.log("Hello World");
+//  }
+
