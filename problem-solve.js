e119ff5722a70fd23b00ad4efd6b3652c0a24a62
Shah Alam <mdalam127671@gmail.com>
2025-08-16 00:23:33 +0600

    sum array

--- problem-solve.js
+++ problem-solve.js
@@ -4,3 +4,22 @@
 //     console.log("Hello World");
 //  }
 
+// 2.Sum of Array Elements in JavaScript
+
+// const numbers=[1,2,3,4,5,6,7];
+// function arraySum(arr){
+//     let sum=0;
+//     for(let num of arr){
+//         sum+=num;
+//     }
+//     return sum;
+// }
+// console.log(arraySum(numbers))
+
+// or................
+
+// const numbersOr=[1,2,3,4,5,6,7];
+// function arraySumOr(arr){
+//     return arr.reduce((sum,num)=>sum+num,0)
+// }
+// console.log(arraySumOr(numbersOr))
